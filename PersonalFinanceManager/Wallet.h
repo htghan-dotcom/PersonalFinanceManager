0eaf8234d723baebbdd810d20ead1d3feb6ab607
mnhat249 <dmnhat2533@apcs.fitus.edu.vn>
2025-12-17 21:42:47 +0700

    Update Wallet.h

--- PersonalFinanceManager/Wallet.h
+++ PersonalFinanceManager/Wallet.h
@@ -13,7 +13,8 @@ struct Wallet {
 
 int findWalletIndexByID(const Wallet* wallets, int walletCount, const string& id);
 int askAndFindWalletIndexByID(const Wallet* wallets, int walletCount);
-void addWallet(Wallet* wallets, int walletCount, Wallet NewWallet);
-void editWallet(Wallet* wallets, int walletCount);
-void deleteWallet(Wallet** wallets, int* walletCount);
-void updateWalletBalance(Wallet* wallets, int walletCount, int walletID, double amount);
+bool isValidWalletID(const string& id);
+void addWallet(Wallet* & wallets, int walletCount, Wallet NewWallet);
+void editWallet(Wallet*&wallets, int walletCount);
+void deleteWallet(Wallet*& wallets, int& walletCount);
+void updateWalletBalance(Wallet* wallets, int walletCount, string walletID, double amount);
