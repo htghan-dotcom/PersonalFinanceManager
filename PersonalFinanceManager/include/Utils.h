48a8f211404b5d5de8f944475d8194c4aeb89ea3
mnhat249 <dmnhat2533@apcs.fitus.edu.vn>
2025-12-19 11:39:57 +0700

    Update Utils.h-using std

--- PersonalFinanceManager/include/Utils.h
+++ PersonalFinanceManager/include/Utils.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include <iostream>
 #include <string>
+using namespace std;
 struct Date {
 	int day;
 	int month;
@@ -14,6 +15,6 @@ void printDate(const Date d);
 int compareDate(Date a, Date b); //a sớm hơn là 1,b sớm hơn là -1,bằng là 0
 bool isDateInRange(Date d, Date from, Date to);
 bool isSameMonthYear(Date d, int m, int y);
-int readInt(const std::string& prompt);
-double readDouble(const std::string& prompt);
-std::string readLine(const std::string& prompt);
+int readInt(const string& prompt);
+double readDouble(const string& prompt);
+string readLine(const string& prompt);
